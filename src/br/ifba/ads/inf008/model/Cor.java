707a25e4c8297a019ff0c7b54041ee91b1aeecee
Laade <57509666+Laade@users.noreply.github.com>
2021-07-10 17:52:08 -0300

    Update Cor.java

--- src/br/ifba/ads/inf008/model/Cor.java
+++ src/br/ifba/ads/inf008/model/Cor.java
@@ -1,7 +1,37 @@
 package inf008.model;
 
 public abstract class Cor {
+    
+    private String id;
+	private String descricao;
+	private String simbolo;
+         
+    public Cor(String id, String descricao, String simbolo) {
+		this.setId(id);
+		this.setDescricao(descricao);
+		this.setSimbolo(simbolo);
+	}
+    private void setId(String id) {
+		this.id = id;
+	}
+	private void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	private void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
+	}
+    private String getId() {
+		return this.id;
+	}
+	private String getDescricao() {
+		return this.descricao;
+	}
+	private String getSimbolo() {
+		return this.simbolo;
+	}
+    
     public abstract int getLuminosidade();
     protected abstract int validaCor(int valor);
     public abstract boolean isEqual(Cor cor);
+    
 }
