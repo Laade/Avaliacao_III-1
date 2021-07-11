53d92ef950149cb1fd0f5659ea93fd74edffee5a
Lorena Carvalho <carvalho.lore@gmail.com>
2021-07-10 22:59:17 -0300

    Update CorDAOIF.java

--- src/br/ifba/ads/inf008/controll/CorDAOIF.java
+++ src/br/ifba/ads/inf008/controll/CorDAOIF.java
@@ -5,4 +5,5 @@ import inf008.model.Cor;
 public interface CorDAOIF {
 	//public void salvar(Cor cor) throws Exception;
 	public int pesquisarCor(Cor c) throws Exception;
+	void adicionar(Cor c) throws Exception;
 }
