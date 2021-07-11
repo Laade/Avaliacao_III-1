ef197d8b319c8e81f4b3841651cb806d8fa5b02c
reinanS <reinandesouza01@gmail.com>
2021-07-11 11:05:12 -0300

    ref: atualiza CorDAOIF

--- src/br/ifba/ads/inf008/controll/CorDAOIF.java
+++ src/br/ifba/ads/inf008/controll/CorDAOIF.java
@@ -1,9 +1,20 @@
 package inf008.controll;
 
+import java.util.Collection;
+
 import inf008.model.Cor;
 
+// Vai ser usado para manipulação com o banco
 public interface CorDAOIF {
-	//public void salvar(Cor cor) throws Exception;
-	public int pesquisarCor(Cor c) throws Exception;
-	void adicionar(Cor c) throws Exception;
+	public void salvar(Cor c) throws Exception;
+	public void atualizar(Cor c) throws Exception;
+	public Collection<Cor> findBySimbolo(String s) throws Exception;
+	public Cor findByNome(String s) throws Exception;
+	public Collection<Cor> findAll() throws Exception;
+
+
+	// //public void salvar(Cor cor) throws Exception;
+	// public void adicionar(Cor c) throws Exception;
+	// public int pesquisarSimbolo(String simbolo) throws Exception;
+
 }
