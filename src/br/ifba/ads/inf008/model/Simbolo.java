8265bfbb09e23283edf976382d8cb4223ff97136
Lorena Carvalho <carvalho.lore@gmail.com>
2021-07-12 15:17:46 -0300

    Add files via upload

---
+++ src/br/ifba/ads/inf008/model/Simbolo.java
@@ -0,0 +1,13 @@
+package inf008.model;
+
+public class Simbolo {
+	int id;
+	String descricao;
+	
+	public Simbolo(int id, String descricao) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+}
