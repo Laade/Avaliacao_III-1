3d85438bb1ab2176dc1c320809f71c6e36a24770
reinanS <reinandesouza01@gmail.com>
2021-07-12 17:22:05 -0300

    ref: altera modificadores de acesso na model Simbolo

--- src/br/ifba/ads/inf008/model/Simbolo.java
+++ src/br/ifba/ads/inf008/model/Simbolo.java
@@ -9,20 +9,20 @@ public class Simbolo {
 		this.setDescricao(descricao);
 	}
 
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	private void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getDescricao() {
 		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 
 }
