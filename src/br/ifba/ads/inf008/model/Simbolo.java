e05d2c128c248b668139d8d8f254ce67f7fbf6c7
Lorena Carvalho <carvalho.lore@gmail.com>
2021-07-12 15:38:02 -0300

    Add files via upload

--- src/br/ifba/ads/inf008/model/Simbolo.java
+++ src/br/ifba/ads/inf008/model/Simbolo.java
@@ -1,12 +1,27 @@
 package inf008.model;
 
 public class Simbolo {
-	int id;
-	String descricao;
+	private int id;
+	private String descricao;
 	
 	public Simbolo(int id, String descricao) {
-		// TODO Auto-generated constructor stub
+		this.setId(id);
+		this.setDescricao(descricao);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
